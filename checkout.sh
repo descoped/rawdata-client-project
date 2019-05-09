979f3d463d19fd0c71108e63cc9de95b4e987341
oranheim <oranheim@gmail.com>
2019-05-09 13:51:35 +0200

    Initial import

---
+++ checkout.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "git clone git@github.com:statisticsnorway/rawdata-client-api.git"
+git clone git@github.com:statisticsnorway/rawdata-client-api.git
+
+echo "git clone git@github.com:statisticsnorway/service-provider-api.git"
+git clone git@github.com:statisticsnorway/service-provider-api.git
+
+echo "git clone git@github.com:statisticsnorway/rawdata-state-provider-h2.git"
+git clone git@github.com:statisticsnorway/rawdata-state-provider-h2.git
+
+echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-local-filesystem.git"
+git clone git@github.com:statisticsnorway/rawdata-client-provider-local-filesystem.git
+
---
+++ pull.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "git -C rawdata-client-api/ pull --rebase"
+git -C rawdata-client-api/ pull --rebase
+
+echo "git -C rawdata-client-provider-local-filesystem/ pull --rebase"
+git -C rawdata-client-provider-local-filesystem/ pull --rebase
+
+echo "git -C rawdata-state-provider-h2/ pull --rebase"
+git -C rawdata-state-provider-h2/ pull --rebase
+
+echo "git -C service-provider-api/ pull --rebase"
+git -C service-provider-api/ pull --rebase

5184a0f9ecd97cc43b05fb012f0078adefe06309
oranheim <oranheim@gmail.com>
2019-05-24 13:25:01 +0200

    Removed duplicate

--- checkout.sh
+++ checkout.sh
@@ -12,9 +12,6 @@ git clone git@github.com:statisticsnorway/rawdata-state-provider-h2.git
 echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-local-filesystem.git"
 git clone git@github.com:statisticsnorway/rawdata-client-provider-local-filesystem.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-local-filesystem.git"
-git clone git@github.com:statisticsnorway/rawdata-client-provider-local-filesystem.git
-
 echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-kafka.git"
 git clone git@github.com:statisticsnorway/rawdata-client-provider-kafka.git
 

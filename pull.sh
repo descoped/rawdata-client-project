83e48cb1674960225bfd32fbe87b20325969cfd3
rupinder <Rupinder.Kaur@ssb.no>
2019-05-24 13:17:32 +0200

    Merged Kafka provider

--- pull.sh
+++ pull.sh
@@ -6,6 +6,9 @@ git -C rawdata-client-api/ pull --rebase
 echo "git -C rawdata-client-provider-local-filesystem/ pull --rebase"
 git -C rawdata-client-provider-local-filesystem/ pull --rebase
 
+echo "git -C rawdata-client-provider-kafka/ pull --rebase"
+git -C rawdata-client-provider-kafka/ pull --rebase
+
 echo "git -C rawdata-state-provider-h2/ pull --rebase"
 git -C rawdata-state-provider-h2/ pull --rebase
 

3c5a9bde7a3e9dcf63359a78923075cb1f9525aa
oranheim <oranheim@gmail.com>
2019-05-28 07:56:46 +0200

    Updated scripts

--- checkout.sh
+++ checkout.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-api.git"
-git clone git@github.com:statisticsnorway/rawdata-client-api.git
-
 echo "git clone git@github.com:statisticsnorway/service-provider-api.git"
 git clone git@github.com:statisticsnorway/service-provider-api.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-state-provider-h2.git"
-git clone git@github.com:statisticsnorway/rawdata-state-provider-h2.git
+echo "git clone git@github.com:statisticsnorway/rawdata-client-api.git"
+git clone git@github.com:statisticsnorway/rawdata-client-api.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-local-filesystem.git"
-git clone git@github.com:statisticsnorway/rawdata-client-provider-local-filesystem.git
+echo "git clone git@github.com:statisticsnorway/rawdata-client-state-provider-h2.git"
+git clone git@github.com:statisticsnorway/rawdata-client-state-provider-h2.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-kafka.git"
-git clone git@github.com:statisticsnorway/rawdata-client-provider-kafka.git
+echo "git clone git@github.com:statisticsnorway/rawdata-client-streams-provider-kafka.git"
+git clone git@github.com:statisticsnorway/rawdata-client-streams-provider-kafka.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-gcp-filestore.git"
-git clone git@github.com:statisticsnorway/rawdata-client-provider-gcp-filestore.git
+echo "git clone git@github.com:statisticsnorway/rawdata-client-storage-provider-local-filesystem.git"
+git clone git@github.com:statisticsnorway/rawdata-client-storage-provider-local-filesystem.git
+
+echo "git clone git@github.com:statisticsnorway/rawdata-client-storage-provider-gcp-filestore.git"
+git clone git@github.com:statisticsnorway/rawdata-client-storage-provider-gcp-filestore.git
--- pull.sh
+++ pull.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+echo "git -C service-client-provider-api/ pull --rebase"
+git -C service-provider-api/ pull --rebase
+
 echo "git -C rawdata-client-api/ pull --rebase"
 git -C rawdata-client-api/ pull --rebase
 
-echo "git -C rawdata-client-provider-local-filesystem/ pull --rebase"
-git -C rawdata-client-provider-local-filesystem/ pull --rebase
+echo "git -C rawdata-client-state-provider-h2/ pull --rebase"
+git -C rawdata-client-state-provider-h2/ pull --rebase
 
-echo "git -C rawdata-client-provider-kafka/ pull --rebase"
-git -C rawdata-client-provider-kafka/ pull --rebase
+echo "git -C rawdata-client-streams-provider-kafka/ pull --rebase"
+git -C rawdata-client-streams-provider-kafka/ pull --rebase
 
-echo "git -C rawdata-state-provider-h2/ pull --rebase"
-git -C rawdata-state-provider-h2/ pull --rebase
+echo "git -C rawdata-client-storage-provider-local-filesystem/ pull --rebase"
+git -C rawdata-client-storage-provider-local-filesystem/ pull --rebase
 
-echo "git -C service-provider-api/ pull --rebase"
-git -C service-provider-api/ pull --rebase
+echo "git -C rawdata-client-storage-provider-gcp-filestore/ pull --rebase"
+git -C rawdata-client-storage-provider-gcp-filestore/ pull --rebase

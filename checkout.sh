b07ff4fd9c34d447cff8caab975dc08eda65cac3
oranheim <oranheim@gmail.com>
2019-10-02 11:22:28 +0200

    Updated scripts

--- checkout.sh
+++ checkout.sh
@@ -21,22 +21,5 @@ git clone git@github.com:statisticsnorway/rawdata-client-provider-lmdb.git
 echo "git clone git@github.com:statisticsnorway/rawdata-copy-util.git"
 git clone git@github.com:statisticsnorway/rawdata-copy-util.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-streams-provider-kafka.git"
-git clone git@github.com:statisticsnorway/rawdata-client-streams-provider-kafka.git
-
-# Decomissioned
-
-echo "git clone git@github.com:statisticsnorway/rawdata-client-state-provider-h2.git"
-git clone git@github.com:statisticsnorway/rawdata-client-state-provider-h2.git
-
-echo "git clone git@github.com:statisticsnorway/rawdata-client-state-provider-postgres.git"
-git clone git@github.com:statisticsnorway/rawdata-client-state-provider-postgres.git
-
-echo "git clone git@github.com:statisticsnorway/rawdata-client-storage-provider-local-filesystem.git"
-git clone git@github.com:statisticsnorway/rawdata-client-storage-provider-local-filesystem.git
-
-echo "git clone git@github.com:statisticsnorway/rawdata-client-storage-provider-gcp-filestore.git"
-git clone git@github.com:statisticsnorway/rawdata-client-storage-provider-gcp-filestore.git
-
-echo "git clone git@github.com:statisticsnorway/rawdata-backup-local-filesystem.git"
-git clone git@github.com:statisticsnorway/rawdata-backup-local-filesystem.git
+echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-kafka.git"
+git clone git@github.com:statisticsnorway/rawdata-client-provider-kafka.git
--- pull.sh
+++ pull.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "git -C . pull --rebase"
+git -C ./ pull --rebase
+
 echo "git -C service-client-provider-api/ pull --rebase"
 git -C service-provider-api/ pull --rebase
 
@@ -21,24 +24,6 @@ git -C rawdata-client-provider-lmdb/ pull --rebase
 echo "git -C rawdata-copy-util/ pull --rebase"
 git -C rawdata-copy-util/ pull --rebase
 
-echo "git -C rawdata-client-streams-provider-kafka/ pull --rebase"
-git -C rawdata-client-streams-provider-kafka/ pull --rebase
-
-
-# Decomissioned
-
-echo "git -C rawdata-client-state-provider-h2/ pull --rebase"
-git -C rawdata-client-state-provider-h2/ pull --rebase
-
-echo "git -C rawdata-client-state-provider-postgres/ pull --rebase"
-git -C rawdata-client-state-provider-postgres/ pull --rebase
-
-echo "git -C rawdata-client-storage-provider-local-filesystem/ pull --rebase"
-git -C rawdata-client-storage-provider-local-filesystem/ pull --rebase
-
-echo "git -C rawdata-client-storage-provider-gcp-filestore/ pull --rebase"
-git -C rawdata-client-storage-provider-gcp-filestore/ pull --rebase
-
-echo "git -C rawdata-backup-local-filesystem/ pull --rebase"
-git -C rawdata-backup-local-filesystem/ pull --rebase
+echo "git -C rawdata-client-provider-kafka/ pull --rebase"
+git -C rawdata-client-provider-kafka/ pull --rebase
 

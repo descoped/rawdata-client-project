fe2d9b21393565c3c46536d46012964bf5b3daac
oranheim <oranheim@gmail.com>
2024-09-21 15:59:03 +0200

    Merge to Descoped

--- checkout.sh
+++ checkout.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-echo "git clone git@github.com:statisticsnorway/service-provider-api.git"
-git clone git@github.com:statisticsnorway/service-provider-api.git
+#echo "git clone git@github.com:descoped/service-provider-api.git"
+#git clone git@github.com:descoped/service-provider-api.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-api.git"
-git clone git@github.com:statisticsnorway/rawdata-client-api.git
+echo "git clone git@github.com:descoped/rawdata-client-api.git"
+git clone git@github.com:descoped/rawdata-client-api.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-postgres.git"
-git clone git@github.com:statisticsnorway/rawdata-client-provider-postgres.git
+echo "git clone git@github.com:descoped/rawdata-client-provider-postgres.git"
+git clone git@github.com:descoped/rawdata-client-provider-postgres.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-pulsar.git"
-git clone git@github.com:statisticsnorway/rawdata-client-provider-pulsar.git
+echo "git clone git@github.com:descoped/rawdata-client-provider-pulsar.git"
+git clone git@github.com:descoped/rawdata-client-provider-pulsar.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-gcs.git"
-git clone git@github.com:statisticsnorway/rawdata-client-provider-gcs.git
+echo "git clone git@github.com:descoped/rawdata-client-provider-gcs.git"
+git clone git@github.com:descoped/rawdata-client-provider-gcs.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-lmdb.git"
-git clone git@github.com:statisticsnorway/rawdata-client-provider-lmdb.git
+echo "git clone git@github.com:descoped/rawdata-client-provider-lmdb.git"
+git clone git@github.com:descoped/rawdata-client-provider-lmdb.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-copy-util.git"
-git clone git@github.com:statisticsnorway/rawdata-copy-util.git
+echo "git clone git@github.com:descoped/rawdata-copy-util.git"
+git clone git@github.com:descoped/rawdata-copy-util.git
 
-echo "git clone git@github.com:statisticsnorway/rawdata-client-provider-kafka.git"
-git clone git@github.com:statisticsnorway/rawdata-client-provider-kafka.git
+echo "git clone git@github.com:descoped/rawdata-client-provider-kafka.git"
+git clone git@github.com:descoped/rawdata-client-provider-kafka.git
--- pull.sh
+++ pull.sh
@@ -3,8 +3,8 @@
 echo "git -C . pull --rebase"
 git -C ./ pull --rebase
 
-echo "git -C service-client-provider-api/ pull --rebase"
-git -C service-provider-api/ pull --rebase
+#echo "git -C service-provider-api/ pull --rebase"
+#git -C service-provider-api/ pull --rebase
 
 echo "git -C rawdata-client-api/ pull --rebase"
 git -C rawdata-client-api/ pull --rebase

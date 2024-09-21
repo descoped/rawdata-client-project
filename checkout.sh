7ab1ef0a8e581dabec8e2a42146997ea69abb3e8
oranheim <oranheim@gmail.com>
2024-09-21 16:22:57 +0200

    Removed deps for Pulasr, Kafka and LMDB (outdated)

--- checkout.sh
+++ checkout.sh
@@ -9,17 +9,8 @@ git clone git@github.com:descoped/rawdata-client-api.git
 echo "git clone git@github.com:descoped/rawdata-client-provider-postgres.git"
 git clone git@github.com:descoped/rawdata-client-provider-postgres.git
 
-echo "git clone git@github.com:descoped/rawdata-client-provider-pulsar.git"
-git clone git@github.com:descoped/rawdata-client-provider-pulsar.git
-
 echo "git clone git@github.com:descoped/rawdata-client-provider-gcs.git"
 git clone git@github.com:descoped/rawdata-client-provider-gcs.git
 
-echo "git clone git@github.com:descoped/rawdata-client-provider-lmdb.git"
-git clone git@github.com:descoped/rawdata-client-provider-lmdb.git
-
 echo "git clone git@github.com:descoped/rawdata-copy-util.git"
 git clone git@github.com:descoped/rawdata-copy-util.git
-
-echo "git clone git@github.com:descoped/rawdata-client-provider-kafka.git"
-git clone git@github.com:descoped/rawdata-client-provider-kafka.git
